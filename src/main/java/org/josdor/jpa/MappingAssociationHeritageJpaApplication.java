e24208f5089a702697430e1af59f919374991e16
Razafinirina <Josdor>
2022-01-28 08:46:06 +0100

    [feature-R1]: initial spring boot project

---
+++ src/main/java/org/josdor/jpa/MappingAssociationHeritageJpaApplication.java
@@ -0,0 +1,13 @@
+package org.josdor.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingAssociationHeritageJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MappingAssociationHeritageJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/josdor/jpa/MappingAssociationHeritageJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.josdor.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingAssociationHeritageJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
